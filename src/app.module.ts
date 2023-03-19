942125088fcd2e37a046395757123e0b751c2511
gustavo poli <poli.gpls@gmail.com>
2023-03-19 08:52:12 -0300

    Configure TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+        host: 'localhost',
+        port: 5432,
+        username: 'user_db',
+        password: 'pass_db',
+        database: 'db_reports',
+        type: 'postgres'     
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
